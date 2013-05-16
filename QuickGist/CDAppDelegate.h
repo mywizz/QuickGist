7abdb8c0b0579800f36f91b6f4dc1817eeda1329
Rob Johnson <corndogcomputers@gmail.com>
2013-05-15 18:10:54 -0700

    Initial Commit

---
+++ QuickGist/CDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDAppDelegate.h
+//  QuickGist
+//
+//  Created by Rob Johnson on 5/15/13.
+//  Copyright (c) 2013 CornDog Computers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CDAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ QuickGist/CDAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CDAppDelegate.m
+//  QuickGist
+//
+//  Created by Rob Johnson on 5/15/13.
+//  Copyright (c) 2013 CornDog Computers. All rights reserved.
+//
+
+#import "CDAppDelegate.h"
+
+@implementation CDAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
