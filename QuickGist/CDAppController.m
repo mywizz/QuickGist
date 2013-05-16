310ad313d95fa43bd20e07a30d357a01c94ebe6b
Rob Johnson <corndogcomputers@gmail.com>
2013-05-16 15:50:08 -0700

    Another test

--- QuickGist/CDAppController.m
+++ QuickGist/CDAppController.m
@@ -36,7 +36,7 @@
 #import "CDAppController.h"
 #import "CDStatusView.h"
 #import "GitHub.h"
-#import "Config.h" /** REMOVE THIS LINE */
+#import "Config.h"
 
 /** ----------------------------------------------------------------------- */
 #ifndef CONFIG
